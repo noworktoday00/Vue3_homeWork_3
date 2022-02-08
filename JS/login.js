804d4a791f3e7a6f38243cb9a3be6afc114f2222
VCShen <37541252+noworktoday00@users.noreply.github.com>
2022-02-08 14:28:52 +0800

    環境準備

--- JS/login.js
+++ JS/login.js
@@ -1,22 +1,22 @@
-import {createApp} from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
 createApp({
-    data(){
-        return{
-            apiUrl:'https://vue3-course-api.hexschool.io/v2',
-            user:{
-                username:"",
-                password:""
+    data() {
+        return {
+            apiUrl: 'https://vue3-course-api.hexschool.io/v2',
+            user: {
+                username: "",
+                password: ""
             }
         }
     },
-    methods:{
-        login(){
+    methods: {
+        login() {
             const api = `${this.apiUrl}/admin/signin`;
-            axios.post(api,this.user).then(res=>{
-                const{token,expired}= res.data;
+            axios.post(api, this.user).then(res => {
+                const { token, expired } = res.data;
                 document.cookie = `vansToken=${token};expires=${new Date(expired)};path=/`;
-                window.location='products.html';
-            }).catch(err=>{
+                window.location = 'products.html';
+            }).catch(err => {
                 alert(err.data.message);
             })
         }
---
+++ JS/products.js
@@ -0,0 +1,18 @@
+import {createApp} from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js";
+
+let productModal = null;
+let delProductModal = null;
+
+createApp({
+    data(){
+
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    }
+})
+
+app.mount("#app");
