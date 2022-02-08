f67b5160a04004aa0574ebbb4dca9a193a568281
VCShen <37541252+noworktoday00@users.noreply.github.com>
2022-02-08 22:39:16 +0800

    why 400 err?

--- JS/products.js
+++ JS/products.js
@@ -1,18 +1,63 @@
-import {createApp} from "https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js";
+import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.esm-browser.js';
 
-let productModal = null;
-let delProductModal = null;
+const apiUrl = 'https://vue3-course-api.hexschool.io/v2/';
+const api_path = 'vanmoritz';
+let productModal = {};
 
-createApp({
-    data(){
-
+const app = createApp({
+    data() {
+        return {
+            products:[],
+            tempProduct:{
+                imagesUrl:[],
+            },
+        }
     },
-    methods:{
-
+    methods: {
+        checkLogin() {
+            //提取token
+            const token = document.cookie.replace(/(?:(?:^|.*;\s*)vansToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+            axios.defaults.headers.common['Authorization'] = token;
+            // console.log(token);
+            const url = `${apiUrl}api/user/check`;
+            axios.post(url)
+                .then(res => {
+                    // console.log(res);
+                    this.getProducts()
+                })
+                .catch(err =>{
+                    alert(err.data.message);
+                    window.location = '.login.html';
+                })
+        },
+        getProducts(){
+            const url = `${apiUrl}api/${api_path}/admin/products/all`;
+            axios.get(url)
+            .then(res=>{
+                console.log(res);
+                this.products = res.data.products;
+            })
+        },
+        openModal(){
+            productModal.show();
+        },
+        addProduct(){
+            console.log(this.tempProduct)
+            const url = `${apiUrl}api/${api_path}/admin/product`;
+            axios.post(url, {"data":this.tempProduct})
+            .then(res=>{
+                console.log(res);
+                this.getProducts();
+                productModal.hide();
+            }).catch(err=>{
+                alert(err.data.message);
+            })
+        }
     },
-    mounted(){
-
-    }
+    mounted() {
+        this.checkLogin();
+        productModal = new bootstrap.Modal(document.getElementById('productModal'));
+    },
 })
 
-app.mount("#app");
+    .mount('#app')
