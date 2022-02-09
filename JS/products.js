9b2a8f866846487a3e765c2fb39884afef1ec69d
VCShen <37541252+noworktoday00@users.noreply.github.com>
2022-02-10 00:01:26 +0800

    基本完成

--- JS/products.js
+++ JS/products.js
@@ -3,14 +3,16 @@ import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.0.9/vue.
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2/';
 const api_path = 'vanmoritz';
 let productModal = {};
+let delProductModal = {};
 
 const app = createApp({
     data() {
         return {
-            products:[],
-            tempProduct:{
-                imagesUrl:[],
+            products: [],
+            tempProduct: {
+                imagesUrl: [],
             },
+            isNew: '',
         }
     },
     methods: {
@@ -25,31 +27,65 @@ const app = createApp({
                     // console.log(res);
                     this.getProducts()
                 })
-                .catch(err =>{
+                .catch(err => {
                     alert(err.data.message);
                     window.location = '.login.html';
                 })
         },
-        getProducts(){
+        getProducts() {
             const url = `${apiUrl}api/${api_path}/admin/products/all`;
             axios.get(url)
-            .then(res=>{
-                console.log(res);
-                this.products = res.data.products;
-            })
+                .then(res => {
+                    console.log(res);
+                    this.products = res.data.products;
+                }).catch(err => {
+                    alert(err.message);
+                })
         },
-        openModal(){
-            productModal.show();
+        openModal(status, product) {
+            console.log(status, product)
+            if (status === 'addProduct') {
+                this.tempProduct = {
+                    imagesUrl: [],
+                }
+                this.isNew = true;
+                productModal.show();
+            } else if (status === 'editProduct') {
+                //注意物件傳參考特性 要做展開淺拷貝
+                this.tempProduct = { ...product };
+                this.isNew = false;
+                productModal.show();
+            } else if (status === 'delProduct'){
+                this.tempProduct = {...product};
+                delProductModal.show();
+            }
         },
-        addProduct(){
+        updateProduct() {
+            //把新增&編輯合併起來
             console.log(this.tempProduct)
-            const url = `${apiUrl}api/${api_path}/admin/product`;
-            axios.post(url, {"data":this.tempProduct})
-            .then(res=>{
-                console.log(res);
+            let url = `${apiUrl}api/${api_path}/admin/product`;
+            let method = 'post';
+            if (!this.isNew) {
+                url = `${apiUrl}api/${api_path}/admin/product/${this.tempProduct.id}`;
+                method = 'put';
+            }
+            axios[method](url, { data: this.tempProduct })
+                .then(res => {
+                    console.log(res);
+                    this.getProducts();
+                    alert(res.data.message);
+                    productModal.hide();
+                }).catch(err => {
+                    alert(err.message);
+                })
+        },
+        delProduct() {
+            const url = `${apiUrl}api/${api_path}/admin/product/${this.tempProduct.id}`;
+            axios.delete(url).then(res => {
+                alert(res.data.message);
+                delProductModal.hide();
                 this.getProducts();
-                productModal.hide();
-            }).catch(err=>{
+            }).catch(err => {
                 alert(err.message);
             })
         }
@@ -57,6 +93,7 @@ const app = createApp({
     mounted() {
         this.checkLogin();
         productModal = new bootstrap.Modal(document.getElementById('productModal'));
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'));
     },
 })
 
